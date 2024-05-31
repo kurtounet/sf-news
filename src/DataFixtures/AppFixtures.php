ee75c2591272746019acc47f163a248849f72df9
Lucas Delobelle <lucas@ld-web.net>
2024-05-31 12:37:47 +0200

    :sparkles: Articles fixtures (Faker)

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,21 +4,30 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use DateTime;
+use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+    private const NB_ARTICLES = 40;
+
     public function load(ObjectManager $manager): void
     {
-        $article = new Article();
-        $article
-            ->setTitle("Mon premier article")
-            ->setContent("Mon contenu")
-            ->setCreatedAt(new DateTime())
-            ->setVisible(true);
+        $faker = Factory::create('zh_TW');
 
-        $manager->persist($article);
+        for ($i = 0; $i < self::NB_ARTICLES; $i++) {
+            $article = new Article();
+            $article
+                ->setTitle($faker->words($faker->numberBetween(4, 7), true))
+                ->setContent($faker->realTextBetween(400, 1500))
+                ->setCreatedAt(DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-2 years')))
+                ->setVisible($faker->boolean(80));
+
+            $manager->persist($article);
+        }
+
         // Envoyer les modifications en base de données
         $manager->flush();
     }
