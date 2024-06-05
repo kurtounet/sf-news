21f83319c024e2733e780447349958f850a81cab
Lucas Delobelle <lucas@ld-web.net>
2024-06-05 16:35:32 +0200

    :sparkles: Article formulaire

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ArticleController extends AbstractController
+{
+    #[Route('/articles', name: 'articles_list')]
+    public function list(ArticleRepository $articleRepository): Response
+    {
+        return $this->render('article/list.html.twig', [
+            'articles' => $articleRepository->findBy([], ['createdAt' => 'DESC'])
+        ]);
+    }
+
+    #[Route('/article/new', name: 'article_new')]
+    public function new(Request $req, EntityManagerInterface $em): Response
+    {
+        $article = new Article();
+        $form = $this->createForm(ArticleType::class, $article);
+
+        $form->handleRequest($req);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setCreatedAt(new \DateTime());
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirectToRoute('articles_list');
+        }
+
+        return $this->render('article/new.html.twig', [
+            'articleForm' => $form
+        ]);
+    }
+}
