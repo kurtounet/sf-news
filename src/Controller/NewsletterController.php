1b2db41fd8cda8d056661d25427cc6889120cc64
Lucas Delobelle <lucas@ld-web.net>
2024-06-06 16:14:07 +0200

    :sparkles: Envoi d'email (synchrone)

--- src/Controller/NewsletterController.php
+++ src/Controller/NewsletterController.php
@@ -8,13 +8,18 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Attribute\Route;
 
 class NewsletterController extends AbstractController
 {
     #[Route('/newsletter/subscribe', name: 'newsletter_subscribe')]
-    public function subscribe(Request $request, EntityManagerInterface $em): Response
-    {
+    public function subscribe(
+        Request $request,
+        EntityManagerInterface $em,
+        MailerInterface $mailer
+    ): Response {
         $newsletterEmail = new NewsletterEmail();
         $form = $this->createForm(NewsletterType::class, $newsletterEmail);
 
@@ -23,6 +28,14 @@ class NewsletterController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $em->persist($newsletterEmail);
             $em->flush();
+
+            $email = (new Email())
+            ->from('admin@hbcorp.com')
+            ->to($newsletterEmail->getEmail())
+            ->subject('Inscription à la newsletter')
+            ->text('Votre email ' . $newsletterEmail->getEmail() . ' a bien été enregistré, merci');
+
+            $mailer->send($email);
 
             return $this->redirectToRoute('newsletter_thanks');
         }
