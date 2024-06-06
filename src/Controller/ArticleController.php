fb488e16f56bc4972174aa40e11a9483c3cfc8d7
Lucas Delobelle <lucas@ld-web.net>
2024-06-06 10:31:30 +0200

    feat: :boom: Add notifications (flash messages)

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -34,6 +34,8 @@ class ArticleController extends AbstractController
             $em->persist($article);
             $em->flush();
 
+            $this->addFlash('success', 'Article enregistré');
+
             return $this->redirectToRoute('articles_list');
         }
 
