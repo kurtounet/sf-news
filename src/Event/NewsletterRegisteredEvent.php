94b519bcd01e094a0285d9ed355bad714f8b6ac0
Anthony bossut <bossut.anthony@gmail.com>
2024-06-18 11:03:06 +0200

    Update

--- src/Event/NewsletterRegisteredEvent.php
+++ src/Event/NewsletterRegisteredEvent.php
@@ -1,32 +1,9 @@
-<<<<<<< HEAD
-<?PHP
-=======
 <?php
 
->>>>>>> 305e96dee4b9b3e423226dbe4a7a3e1c03370fe0
 namespace App\Event;
 
 use App\Entity\NewsletterEmail;
 
-<<<<<<< HEAD
-
-// NewsletterRegisteredSubcriber -> php bin/console make:subscriber
-
-class NewsletterRegisteredEvent
-{
-    public const NAME = 'newsletter.registered';
-    public function __construct(
-        private NewsletterEmail $email
-
-    ) {
-
-    }
-    public function getEmail()
-    {
-        return $this->email;
-    }
-}
-=======
 class NewsletterRegisteredEvent
 {
     public const NAME = 'newsletter.registered';
@@ -41,4 +18,3 @@ class NewsletterRegisteredEvent
         return $this->email;
     }
 }
->>>>>>> 305e96dee4b9b3e423226dbe4a7a3e1c03370fe0
